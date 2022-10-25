bda833bea867ad28ea44b43c2891eb5f87b77930
liana13555 <barishovaliana@gmail.com>
2022-10-25 13:43:25 +0200

    Add example

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Counter from './Counter';
+
+const App = () => (
+  <>
+    <h1>React with TypeScript </h1>
+        <Counter initialValue={10}/>
+  </>
+)
+
+
+export default App;
